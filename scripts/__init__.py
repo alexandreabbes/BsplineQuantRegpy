7786e3ba2118347f53acef3576c9dcbc8ef4d476
alexandreabbes <al.abbes@laposte.net>
2026-07-14 14:07:17 +0200

    RTD 	supprimé :        readthedocs.yaml 	supprimé :        scripts/__init__.py 	supprimé :        scripts/run_gui.py 	supprimé :        scripts/run_gui.py~ 	modifié :         src/BsplineQuantRegpy/__pycache__/__init__.cpython-313.pyc 	supprimé :        src/BsplineQuantRegpy/core/run_gui.py 	modifié :         src/BsplineQuantRegpy/gui/__pycache__/Quant_reg_tk.cpython-313.pyc 	modifié :         src/BsplineQuantRegpy/models/__pycache__/__init__.cpython-313.pyc 	modifié :         src/BsplineQuantRegpy/models/__pycache__/quantile_reg.cpython-313.pyc 	.gitignore 	.matlab/ 	files.txt 	setup.cfg

--- scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Scripts d'exécution du package PysplineQuantReg.
-"""
-
-# Ce fichier permet de marquer le dossier comme un package Python
-# mais les scripts ne sont pas importés automatiquement.
